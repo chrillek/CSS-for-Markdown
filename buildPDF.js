aa5836afcd53b66529b7bccf1a42503ec9c3ea0e
chrillek <ck@bru6.de>
2022-06-15 13:15:11 +0200

    buildPDF

--- buildPDF.js
+++ buildPDF.js
@@ -8,9 +8,14 @@
   app.logMessage(`path: ${pathToDirectory}/mdName: ${mdName}/targetDir: ${targetDirectory}`);
    /* import path into new group in the global inbox */
   const mdGroup = app.import(pathToDirectory);
+  app.logMessage('imported')
   const mdFile = app.search(`name: ${mdName}`, {in: mdGroup});
+  app.logMessage(`{mdFile.length} files found in Group`);
   if (mdFile.length === 1) throw `File '${mdName}' not found in group '${mdGroup}'`;
   const pdfFile = app.convert( {record: mdFile[0], to: "PDF document"});
+  app.logMessage(`pdfFile created ${pdfFile.path()}`);
   app.export({record: pdfFile, to: targetDirectory});
+  app.logMessage("pdfFile exported");
   app.delete({record: mdGroup});
+  app.logMessage(`group ${mdGroup.name()} deleted`)
 })()
