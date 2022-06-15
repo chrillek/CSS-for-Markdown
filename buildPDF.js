df1903eb6a4a6456b792fa9d059b95aebdf0ec8d
chrillek <ck@bru6.de>
2022-06-15 13:01:22 +0200

    buildPDF

--- buildPDF.js
+++ buildPDF.js
@@ -5,9 +5,10 @@
   if (!pathToDirectory) throw "Must pass directory name on command line"
   const mdName = args.js[5].js;
   const targetDirectory = args.js[6].js;
-  const mdGroup = app.import(Path(pathToDirectory)) /* import path into new group in the global inbox */
-  const mdFile = app.search(`filename: ${mdName}`, {in: mdGroup});
-  const pdfFile = app.convert( {record: mdFile, to: "PDF document"});
-  app.export({record: pdfFile}, {to: targetDirectory});
+  const mdGroup = app.import(pathToDirectory) /* import path into new group in the global inbox */
+  const mdFile = app.search(`name: ${mdName}`, {in: mdGroup});
+  if (mdFile.length === 1) throw `File '${mdName}' not found in group '${mdGroup}'`;
+  const pdfFile = app.convert( {record: mdFile[0], to: "PDF document"});
+  app.export({record: pdfFile, to: targetDirectory});
   app.delete({record: mdGroup});
 })()
